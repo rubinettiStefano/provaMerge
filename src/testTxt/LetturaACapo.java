5c52d6668e249ea77a06918a5528a81e6dc09240
rubinettiStefano <stefano.rubinetti@gmail.com>
2023-08-31 19:01:39 +0200

    main

---
+++ src/testTxt/LetturaACapo.java
@@ -0,0 +1,16 @@
+package testTxt;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class LetturaACapo
+{
+	public static void main(String[] args) throws FileNotFoundException 
+	{
+		Scanner fileReader = new Scanner(new File("splitConBS.txt"));
+		
+	}
+}
